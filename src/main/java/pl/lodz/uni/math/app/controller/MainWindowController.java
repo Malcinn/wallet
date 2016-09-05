d65959cd568692305f0459705f6c28b9e88be2a5
Marcin Kowalczyk <marcinkowalczyk1992@gmail.com>
2016-09-05 12:06:57 +0200

    Added MainWindow.fxml file and controller class for it, called MainWindowController.java

---
+++ src/main/java/pl/lodz/uni/math/app/controller/MainWindowController.java
@@ -0,0 +1,14 @@
+package pl.lodz.uni.math.app.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MainWindowController implements Initializable {
+
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
--- src/main/java/pl/lodz/uni/math/app/main/Main.java
+++ src/main/java/pl/lodz/uni/math/app/main/Main.java
@@ -1,8 +1,28 @@
 package pl.lodz.uni.math.app.main;
 
-public class Main {
+import static java.io.File.separator;
 
-	public static void main (String[] args){
-		System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		VBox root = (VBox) FXMLLoader.load(getClass().getResource(separator + "fxml" + separator + "MainWindow.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets()
+				.add(getClass().getResource(separator + "styles" + separator + "styles.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Wallet");
+		primaryStage.show();
+
 	}
 }
