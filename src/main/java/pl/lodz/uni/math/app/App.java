d0bd3542c39f11507bd7ba8a024b83f7e53279a2
Marcin Kowalczyk <marcinkowalczyk1992@gmail.com>
2016-09-05 10:59:01 +0200

    initial commit, create project structure, added .gitignore and README.md files.

---
+++ src/main/java/pl/lodz/uni/math/app/App.java
@@ -0,0 +1,13 @@
+package pl.lodz.uni.math.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
